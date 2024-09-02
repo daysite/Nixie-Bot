fcf1866d076563afe74ba17cbf8c68c22d400820
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-02 06:30:26 -0500

    Update buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -6,7 +6,7 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
     const datas = global;
     const device = await getDevice(m.key.id);
     
-  if (!text) throw `⚠️ *Error*`;
+  if (!text) throw `⚠️ *_Que quieres que busque en YouTube?_*`;
     
   if (device !== 'desktop' || device !== 'web') {      
     
@@ -17,10 +17,10 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
 
   var messa = await prepareWAMessageMedia({ image: {url: randomVideo.thumbnail}}, { upload: conn.waUploadToServer })
   const interactiveMessage = {
-    body: { text: `*🍟 Resultados De:* ${text}\n*🍟 Resultados obtenidos:* ${results.videos.length}`.trim() },
-    footer: { text: `•${global.md}`.trim() },  
+    body: { text: `h`.trim() },
+    footer: { text: `${global.wm}`.trim() },  
       header: {
-          title: `*•/• YouTube - Search •/•*`,
+          title: `*❤️‍🔥 Mᴇɢᴜᴍɪɴ Sᴇᴀʀᴄʜ ❤️‍🔥*\n`,
           hasMediaAttachment: true,
           imageMessage: messa.imageMessage,
       },
@@ -29,7 +29,7 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
         {
           name: 'single_select',
           buttonParamsJson: JSON.stringify({
-            title: 'SELECCIONE AQUÍ',
+            title: 'OPCIONES DISPONIBLES',
             sections: videos.map((video) => ({
               title: video.title,
               rows: [
@@ -37,13 +37,13 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                   header: video.title,
                   title: video.author.name,
                   description: 'Descargar MP3',
-                  id: `${prefijo}fgmp3 ${video.url}`
+                  id: `${prefijo}play.1 ${video.url}`
                 },
                 {
                   header: video.title,
                   title: video.author.name,
                   description: 'Descargar MP4',
-                  id: `${prefijo}fgmp4 ${video.url}`
+                  id: `${prefijo}play.2 ${video.url}`
                 }
               ]
             }))
@@ -60,20 +60,24 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                     interactiveMessage,
                 },
             },
-        }, { userJid: conn.user.jid, quoted: m })
+        }, { userJid: conn.user.jid, quoted: fgif2 })
       conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id});
 
   } else {
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender].language;
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
+  const traductor = _translate.plugins.buscador_yts;      
   const results = await yts(text);
   const tes = results.all;
   const teks = results.all.map((v) => {
     switch (v.type) {
       case 'video': return `
 ° *_${v.title}_*
-↳ 🫐 *_Url_* ${v.url}
-↳ 🕒 *_Fecha_* ${v.timestamp}
-↳ 📥 *_fecha_* ${v.ago}
-↳ 👁 *_Vista_* ${v.views}`;
+↳ 🫐 *_Link :_* ${v.url}
+↳ 🕒 *_DuraciÃ³n :_* ${v.timestamp}
+↳ 📥 *_Subido :_* ${v.ago}
+↳ 👁 *_Vistas :_* ${v.views}`;
     }
   }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
   conn.sendFile(m.chat, tes[0].thumbnail, 'error.jpg', teks.trim(), m);      
@@ -81,5 +85,7 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
 };
 handler.help = ['ytsearch <texto>'];
 handler.tags = ['search'];
-handler.command = /^(playlist|yts|searchyt|yts|videosearch|audiosearch)$/i;
+handler.command = ['ytsearch','yts','searchyt','buscaryt','videosearch','audiosearch'];
+handler.register = true;
+handler.group = true;
 export default handler;
