695f8e3ab6967aba74fe09f9e78d242b4efb8d8f
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-01 23:51:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Yaemori ✰');
+console.log('\n✰ Iniciando Nixie ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Yaemori\nBot', {
+say('Nixie\nBot', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • Dev Diego`, {
+say(`Developed By • Daniel`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
