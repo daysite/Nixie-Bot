20b422bdafc996fa3d209220d8d7e3e3c907a384
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-02 00:26:58 -0500

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -19,7 +19,7 @@ let img = catalogo
 					description: null,
 					previewType: "PHOTO",
 					thumbnailUrl: 'https://telegra.ph/file/474de03b8c0cf87231d8e.jpg',
-		           sourceUrl: canal,
+		           sourceUrl: channel,
 		           mediaType: 1,
                    renderLargerThumbnail: false
 	    },
@@ -39,7 +39,7 @@ let img = catalogo
 					previewType: "PHOTO",
 					thumbnailUrl: img,
                     thumbnail: img,
-		           sourceUrl: canal,
+		           sourceUrl: channel,
 		           mediaType: 1,
                    renderLargerThumbnail: true
 				}
