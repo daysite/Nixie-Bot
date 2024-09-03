bd41eb17a27f9965db8c6280b3a44a798158798a
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-03 12:16:41 -0500

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,33 +1,46 @@
-import { snapsave } from '@bochilteam/scraper';
-import fetch from 'node-fetch';
+import { igdl } from 'ruhend-scraper'
 
-let handler = async (m, { conn, args }) => {
-if (!args[0]) return m.reply('*Ingresa un enlace de facebook*');
-
+const handler = async (m, { text, conn, args, usedPrefix, command }) => {
+if (!args[0]) {
+return conn.reply(m.chat, '🍟 *Ingresa Un Link De Facebook*', m, rcanal)}
+let res
 try {
-await m.react('🕓'); 
-const data = await snapsave(args[0]);
-
-if (!Array.isArray(data) || data.length === 0) return m.reply('*No se pudo descargar el video :/*');
-
-let video = data.find(v => v.resolution.includes('HD')) || data[0];
-
-if (video) {
-const videoBuffer = await fetch(video.url).then(res => res.buffer());
-
-await conn.sendMessage( m.chat, { video: videoBuffer, mimetype: 'video/mp4', fileName: 'video.mp4', caption: caption, mentions: [m.sender], m);
-await m.react('✅'); 
-);
-} else {
-await m.react('❌'); 
-}
+/*conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+await m.react(rwait)*/
+res = await igdl(args[0])
 } catch {
-await m.react('❌'); 
-}
-}
+//await m.react(error)
+return conn.reply(m.chat, '🚩 *Error al obtener datos. Verifica el enlace.*', m, rcanal)}
+let result = res.data
+if (!result || result.length === 0) {
+return conn.reply(m.chat, '🚩 *No se encontraron resultados.*', m, rcanal)}
+let data
+try {
+//await m.react(rwait)
+data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)")
+} catch {
+//await m.react(error)
+return conn.reply(m.chat, '🚩 *Error al procesar los datos.*', m, rcanal)}
+if (!data) {
+return conn.reply(m.chat, '🚩 *No se encontró una resolución adecuada.*', m, rcanal)}
+let video = data.url
+try {
+//await m.react(rwait)
+await conn.sendMessage(m.chat, { video: { url: video }, caption: '🍟 *Tu video de facebook.*\n' + textbot, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
+await m.react(done)
+} catch {
+//await m.react(error)
+return conn.reply(m.chat, '🚩 *Error al enviar el video.*', m, rcanal)}}
 
-handler.help = ['facebook *<link>*'];
-handler.tags = ['dl'];
-handler.command = ['fb', 'facebook', 'FB', 'FACEBOOK'];
+handler.help = ['facebook', 'fb']
+handler.tags = ['descargas']
+handler.command = ['facebook', 'fb']
+handler.estrellas = 1
+handler.register = true
 
-export default handler;
+export default handler
