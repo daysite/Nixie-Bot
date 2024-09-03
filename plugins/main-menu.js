cae41a2b877349187c57189f8e7ccc56e1378f8b
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-03 11:41:56 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -81,7 +81,7 @@ await conn.sendMessage(m.chat, {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
 let menu = `  
-*•/• Runa Bot - MD •/•*
+*•/• Nixie Bot - MD •/•*
  
 👤 Creador : Daniel 🇦🇱
 🌍 Pais : Perú 🇵🇪
@@ -152,7 +152,7 @@ console.log(e)
 }}
 handler.help = ['menu', 'help', '?']
 handler.tags = ['main']
-handler.command = /^(allmenu|allmenu\?)$/i
+handler.command = /^(menu|help\?)$/i
 //handler.register = true
 handler.exp = 50
 handler.fail = null
