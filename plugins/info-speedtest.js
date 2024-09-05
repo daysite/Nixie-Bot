d91aeefd5a3cb97bbc9d0a021d9525a4a8ce5b2c
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-04 21:50:47 -0500

    Update info-speedtest.js

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -1,27 +1,32 @@
-import cp from 'child_process';
-import {promisify} from 'util';
-const exec = promisify(cp.exec).bind(cp);
-const handler = async (m) => {
-    conn.reply(m.chat, wait, m, {
-    contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-    title: packname,
-    body: wm,
-    previewType: 0, thumbnail: icons,
-    sourceUrl: channel }}})
-  let o;
-  try {
-    o = await exec('python3 speed.py');
-  } catch (e) {
-    o = e;
-  } finally {
-    const {stdout, stderr} = o;
-    if (stdout.trim()) m.reply(stdout);
-    if (stderr.trim()) m.reply(stderr);
-  }
-};
-handler.help = ['speedtest'];
-handler.tags = ['info'];
-handler.command = ['speedtest', 'info', 'speed'];
+import cp from 'child_process'
+import {promisify} from 'util'
+
+const exec = promisify(cp.exec).bind(cp)
+
+var handler = async (m) => {
+
+conn.reply(m.chat, '☁️ Espere un momento soy lenta..', m)
+
+let o
+  
+try {
+
+o = await exec('python3 speed.py --secure --share');
+} catch (e) {
+o = e
+} finally {
+let {stdout, stderr} = o
+
+
+if (stdout.trim()) conn.reply(m.chat, stdout, m)
+if (stderr.trim()) conn.reply(m.chat, stderr, m)
+}
+
+}
+handler.help = ['speedtest']
+handler.tags = ['bot']
+handler.command = /^(speedtest?|test?speed)$/i
 
 handler.register = true
-export default handler;
+
+export default handler
