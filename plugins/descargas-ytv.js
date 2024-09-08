4f88030c8cbd511ad30784e07914663c4a730e39
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-08 02:18:35 -0500

    Update descargas-ytv.js

--- plugins/descargas-ytv.js
+++ plugins/descargas-ytv.js
@@ -1,21 +1,21 @@
 import { ytmp4, ytmp4v4 } from 'ruhend-scraper'
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!args || !args[0]) return conn.reply(m.chat, '*`Ingresa Un Link De Youtube`*', m)
+if (!args || !args[0]) return conn.reply(m.chat, '`Ingresa Un Link De Youtube`', m)
 if (!args[0].match(/youtu/gi)) return conn.reply(m.chat, '*`Verifica que el enlace sea de YouTube.`*', m).then(_ => m.react('❌'))
     
 try {
 await m.react('🕑')
 const data = await ytmp4(args[0])
 let { title, size, video, quality, thumbnail } = data
-conn.sendFile(m.chat, video, title + '.mp4', `_${title}_\n_${quality}_`, m, false, { mimetype: 'video/mp4', asDocument: false })
+conn.sendFile(m.chat, video, title + '.mp4', `${title}\n${quality}`, m, false, { mimetype: 'video/mp4', asDocument: false })
 await m.react('✅')
 } catch {
 try { 
 await m.react('🕑')
 const data = await ytmp4v4(args[0])
 let { title, video } = data
-conn.sendFile(m.chat, video, title + '.mp4', `_${title}_`.trim(), m, false, { mimetype: 'video/mp4', asDocument: false })
+conn.sendFile(m.chat, video, title + '.mp4', `${title}`.trim(), m, false, { mimetype: 'video/mp4', asDocument: false })
 await m.react('✅')
 } catch {
 await m.react('❌')
