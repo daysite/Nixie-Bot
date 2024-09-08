991a58879619b2b12ae01ee459d0afe66c2ca166
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-08 18:43:45 -0500

    Create plan5.js

---
+++ plugins/plan5.js
@@ -0,0 +1,24 @@
+const handler = async (m, {conn}) => {
+
+conn.reply(m.chat, `🛍️ Precios Plan 2
+
+  •$5.00 USD
+  •5400 AzuraCoins
+
+*🌀Azura Hosting*
+
+Este Plan ya es mas recomendable para el uso de bots con mayor capacidad.
+
+*🔰 ¡Nuestro Enlaces! 🔰*
+
+🌀 Dash:
+• ${global.md}
+🫧 Panel: 
+• ${global.yt}
+
+> ${global.packname}`, m, rcanal)
+
+}
+handler.customPrefix = /plan5|planes5/i
+handler.command = new RegExp
+export default handler
