b910cca4b8d8677170ce73dfebf982a21de9caab
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-08 01:46:06 -0500

    Update plan1-prueba.js

--- plugins/plan1-prueba.js
+++ plugins/plan1-prueba.js
@@ -12,6 +12,6 @@ conn.reply(m.chat, `Plan 1 Prueba. 🌟
 > ${global.packname}`, m, rcanal)
 
 }
-handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i
+handler.customPrefix = /plan1|planes1/i
 handler.command = new RegExp
 export default handler
