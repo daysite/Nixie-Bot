6fa256ca4bc0a64852002f89688f7065a4e15b88
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-08 19:14:36 -0500

    Update descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -2,7 +2,7 @@ import { igdl } from "ruhend-scraper"
 
 let handler = async (m, { args, conn }) => { 
 if (!args[0]) {
-return conn.reply(m.chat, '`Ingresa un link de Instagram`', m, rcanal)}
+return conn.reply(m.chat, 'Ingresa un link de Instagram', m, rcanal)}
 try {
 //await m.react(rwait)
 /*conn.reply(m.chat, `🕒 *Enviando El Video...*`, m, {
@@ -15,7 +15,7 @@ let res = await igdl(args[0])
 let data = res.data       
 for (let media of data) {
 await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '`Aqui tiene`.*\n' + textbot, m)
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '`Aqui tiene`.\n' + textbot, m)
 }} catch {
 await m.react(error)
 conn.reply(m.chat, '🚩 Ocurrió un error.', m, rcanal)}}
