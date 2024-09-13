8e2482c5d1e58b7f94613379aec4aaba1f4b4186
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-12 23:34:14 -0500

    Update buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -37,13 +37,13 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                   header: video.title,
                   title: video.author.name,
                   description: 'Descargar MP3',
-                  id: `${prefijo}fgmp3 ${video.url}`
+                  id: `${prefijo}ytmp3 ${video.url}`
                 },
                 {
                   header: video.title,
                   title: video.author.name,
                   description: 'Descargar MP4',
-                  id: `${prefijo}fgmp4 ${video.url}`
+                  id: `${prefijo}ytmp4 ${video.url}`
                 }
               ]
             }))
