da453a93a9cadab21958c116584b1422edc5b5af
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-09-22 00:11:54 -0500

    Create descargas.tiktok2.js

---
+++ plugins/descargas.tiktok2.js
@@ -0,0 +1,30 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return conn.reply(m.chat, '*Ingresa un enlace de Tiktok*', m);
+ 
+try {
+let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/tiktok2?url=${args[0]}`);
+let json = await api.json();
+let { video, title, likes, comment, shares, views, creator, desc } = json
+
+let txt = `*Titulo* : ${title}
+*Descripcion* : ${desc}
+*Creador* : ${creator}
+*Visitas* : ${views}
+*Likes* : ${likes}
+*Comentarios* : ${comment}
+*Compartidos* : ${shares}
+`;
+await conn.sendFile(m.chat, video, 'tiktok.mp4', txt, m)
+
+await conn.sendMessage(m.chat, { audio: { url: video }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m });
+
+} catch {
+conn.reply('error :v')    
+}
+}
+    
+handler.command = ['tiktok2'];
+
+export default handler;
