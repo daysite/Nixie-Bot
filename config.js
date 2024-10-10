e43c1847dd6bbe2a4ebf76e6eaf5a8d48cdc9176
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-10-09 23:34:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -9,71 +9,42 @@ import moment from 'moment-timezone'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
-//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +573218138672
-global.confirmCode = ''
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
 global.owner = [
-   ['51923300039', 'Dev Daniel', true],
-   ['51994143761', 'Owner', true],
-   ['51994143761']
+  ['51923300039', 'Danịel 🇦🇱⁩', true],
+  ['5491122461521'],
+  ['524434703586'],
+  ['51955918117'],
+  ['5491122461521']
 ]
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.mods = []
 global.prems = []
+   
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = ``
+global.author = '{\n "bot": {\n   "name": "Sumi - Sakurasawa",\n     "author": "Danịel",\n   "status_bot": "active"\n }\n}'
+global.wait = '🐢 *Sumi Sakurasawa*'
+global.botname = 'Sumi - Sakurasawa'
+global.textbot = `Powered By Starlights Team`
+global.listo = '*🍭 Aqui tiene*'
+global.namechannel = '【 ✯ Starlights Team - Oficial Chanel ✰ 】'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-//cambiar a true si el bot detecta sus propios comandos.
-global.isBaileysFail = false
-global.libreria = 'Baileys'
-global.baileys = '@whiskeysockets/baileys'
-global.nameqr = 'Nixie Bot - MD'
-global.sessions = 'MiniSession'
-global.jadi = 'MiniJadiBot'
+global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
+global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = '⪛✰ Nixie - Bot ✰⪜'
-global.botname = '© Nixie Bot - MD'
-global.wm = '© powered by Nixie ☄︎'
-global.author = 'created by Daniel ᰔᩚ'
-global.dev = '© powered by Daniel ⚡︎'
-global.textbot = 'Nixie Bot : Daniel 🚩'
-global.vs = '2.0.6'
+global.group = 'https://chat.whatsapp.com/J1R402WH1N0Hdl3S0NDEYu'
+global.canal = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.imagen1 = fs.readFileSync('./media/Menu.jpg')
-global.imagen2 = fs.readFileSync('./media/Menu2.jpg')
-global.imagen3 = fs.readFileSync('./media/Menu3.jpg')
-global.welcome = fs.readFileSync('./media/welcome.jpg')
-global.adios = fs.readFileSync('./media/adios.jpg')
-global.catalogo = fs.readFileSync('./media/catalogo.jpg')
-global.miniurl = fs.readFileSync('./media/miniurl.jpg')
-global.avatar = fs.readFileSync('./media/avatar_contact.jpg')
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.grupo = 'https://chat.whatsapp.com/G9MpDVmJtbiIpItEZpg70s' //YaemoriBot
-global.grupo2 = 'https://chat.whatsapp.com/G9MpDVmJtbiIpItEZpg70s' //YaemoriBot2
-global.grupo3 = 'https://chat.whatsapp.com/G9MpDVmJtbiIpItEZpg70s' //team oficial
-global.grupo4 = 'https://chat.whatsapp.com/G9MpDVmJtbiIpItEZpg70s' //GataBot & YaemoriBot
-global.channel = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'
-global.channel2 = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'
-global.md = 'https://store.azuraultra-host.pro' 
-global.yt = 'https://control.azuraultra-host.pro'
-global.tiktok = 'https://chat.whatsapp.com/G9MpDVmJtbiIpItEZpg70s'
-global.correo = 'danieldevelop3@gmail.com'
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '❀ sᥙ́⍴ᥱr ᥕһᥲ𝗍sᥲ⍴⍴ ᑲ᥆𝗍 ☄︎', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: botname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
@@ -81,18 +52,18 @@ global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
-global.moment = moment        
+global.moment = moment	
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.multiplier = 69 
-global.maxwarn = '3'
+global.maxwarn = '2' // máxima advertencias
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
-  console.log(chalk.redBright("Update 'settings.js'"))
+  console.log(chalk.redBright("Update 'config.js'"))
   import(`${file}?update=${Date.now()}`)
 })
